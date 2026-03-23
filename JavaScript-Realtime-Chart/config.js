e8d5ac6f697f1171de45b67154e00a13f88c808c
I Gede Surya Rahayuda <surya.rahayuda@gmail.com>
2026-03-23 15:21:11 +0800

    Add Firebase configuration to config.js

--- JavaScript-Realtime-Chart/config.js
+++ JavaScript-Realtime-Chart/config.js
@@ -1,1 +1,11 @@
-
+// config.js
+export const firebaseConfig = {
+    apiKey: "AIzaSyD-4viZ0SJcKW-vRwjDOZtXjhLhXPWXnDw",
+    authDomain: "sensoremulator.firebaseapp.com",
+    databaseURL: "https://sensoremulator-default-rtdb.firebaseio.com",
+    projectId: "sensoremulator",
+    storageBucket: "sensoremulator.appspot.com",
+    messagingSenderId: "410626311115",
+    appId: "1:410626311115:web:24c25cd8ef9e62b62bec4e",
+    measurementId: "G-QESD44RL09"
+};
